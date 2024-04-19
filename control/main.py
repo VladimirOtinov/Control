3c52e1953cfabb4d8dcc04133495b2991156fd43
VladimirOtinov <25050otinov@gmail.com>
2024-04-19 17:10:50 +0300

    all work done

--- control/main.py
+++ control/main.py
@@ -4,9 +4,13 @@ input2 = ["1234", "1567", "-2", "computer science"]
 input3 = ["Russia", "Denmark", "Kazan"]
 
 
-def func(inp = [], outp = []):
+def func(inp = []):
+    outp = []
     for i in range(len(inp)):
-        if len(inp) <= 3:
+        if len(inp[i]) <= 3:
             outp.append(inp[i])
+    return outp
 
-
+print(func(input1))
+print(func(input2))
+print(func(input3))
