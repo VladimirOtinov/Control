5245ace0c64edbeee878cd0c50789442db777375
VladimirOtinov <25050otinov@gmail.com>
2024-04-19 17:06:44 +0300

    added func in project

--- control/main.py
+++ control/main.py
@@ -2,3 +2,11 @@
 input1 = ["Hello", "2", "world", ":-)"]
 input2 = ["1234", "1567", "-2", "computer science"]
 input3 = ["Russia", "Denmark", "Kazan"]
+
+
+def func(inp = [], outp = []):
+    for i in range(len(inp)):
+        if len(inp) <= 3:
+            outp.append(inp[i])
+
+
